6049c21f1b2d1728674b4812039909a88183ffbf
Ryan2301010356 <denfalperbatasan03@gmail.com>
2025-04-08 21:40:25 +0800

    Keterangan commit

---
+++ pertemuan5/src/pertemuan5/Pertemuan5.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan5;
+
+/**
+ *
+ * @author LAB F
+ */
+public class Pertemuan5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        jajan pancong = new jajan();
+        // pancong.bentuk = "lonjong";
+        pancong.setBentuk ("manis");
+        pancong.rasa = "coklat";
+        pancong.harga = 5000;
+        pancong.warna = "pink";
+        
+        System.out.println("rasanya: "+pancong.getBentuk());
+        
+        
+    }
+    
+}
+
+
+
